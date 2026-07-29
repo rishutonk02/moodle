e3a09cc807cf7eb2ce5cbf0c11eab9da0101ddce
rishutonk <rishutonk02@gmail.com>
2026-07-29 12:27:30 +0100

    resolved the extension issue

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:moodle/routes/app_routes.dart';
 import 'package:moodle/services/firebase_service.dart';
-import 'package:moodle/theme/app_theme.dart';
+import 'package:moodle/themes/app_theme.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
