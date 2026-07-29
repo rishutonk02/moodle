0b927af3bec1d6ab0c61e51278bca64ec5966217
rishutonk <rishutonk02@gmail.com>
2026-07-29 13:48:58 +0100

    Created user prfile model for user data

---
+++ lib/models/user_profile.dart
@@ -0,0 +1,11 @@
+class AppUserProfile {
+  const AppUserProfile({
+    required this.name,
+    required this.email,
+    required this.photoUrl,
+  });
+
+  final String name;
+  final String email;
+  final String? photoUrl;
+}
