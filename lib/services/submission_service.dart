d9aac5de19e128cc2ff75481dbed7b669d66ca10
rishutonk <rishutonk02@gmail.com>
2026-07-29 13:52:04 +0100

    submission service

---
+++ lib/services/submission_service.dart
@@ -0,0 +1,10 @@
+import 'package:moodle/models/submission.dart';
+
+class SubmissionService {
+  Future<AssignmentSubmission?> getSubmission(
+      {required String assignmentId, required String userId}) async {
+    return null;
+  }
+
+  Future<void> saveSubmission(AssignmentSubmission submission) async {}
+}
