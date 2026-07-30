2732eb2088a3726236ac9c51f4cd33c3db2696c8
rishutonk02 <rishutonk02@gmail.com>
2026-07-30 08:53:54 +0100

    added up number

--- lib/utils/students_details.dart
+++ lib/utils/students_details.dart
@@ -1,6 +1,6 @@
 class StudentDetails {
   static const String name = 'Rishu Tonk';
-  static const String upNumber = 'TODO: add UP number';
+  static const String upNumber = '2286527';
   static const String email = 'TODO-UP-NUMBER@myport.ac.uk';
   static const String initials = 'RT';
 }
