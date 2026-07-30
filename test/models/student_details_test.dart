4f036ae7a973f00f91bc1df4f981b0df4ac15cbe
rishutonk02 <rishutonk02@gmail.com>
2026-07-30 09:16:08 +0100

    Created test file for students details

---
+++ test/models/student_details_test.dart
@@ -0,0 +1,11 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:moodle/utils/students_details.dart';
+
+void main() {
+  test('student details expose the expected profile values', () {
+    expect(StudentDetails.name, 'Rishu Tonk');
+    expect(StudentDetails.upNumber, '2286527');
+    expect(StudentDetails.initials, 'RT');
+    expect(StudentDetails.email, contains('TODO-UP-NUMBER'));
+  });
+}
