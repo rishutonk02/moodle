d53faf66b386491800d55a73d9729fdf5ed2614d
rishutonk02 <rishutonk02@gmail.com>
2026-07-30 12:13:36 +0100

    added email

--- lib/models/user_profile.dart
+++ lib/models/user_profile.dart
@@ -1,11 +1,33 @@
 class AppUserProfile {
   const AppUserProfile({
+    required this.uid,
     required this.name,
     required this.email,
-    required this.photoUrl,
+    this.photoUrl,
   });
 
+  final String uid;
   final String name;
   final String email;
   final String? photoUrl;
+
+  Map<String, Object?> toFirestore() {
+    return {
+      'uid': uid,
+      'name': name,
+      'email': email,
+      'photoUrl': photoUrl,
+    };
+  }
+
+  factory AppUserProfile.fromFirestore(Map<String, Object?> data) {
+    return AppUserProfile(
+      uid: data['uid'] as String? ?? '',
+      name: data['name'] as String? ?? 'Student',
+      email: data['email'] as String? ?? '',
+      photoUrl: data['photoUrl'] as String?,
+    );
+  }
+
+  get displayName => null;
 }
