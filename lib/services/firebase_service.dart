e98b1119cf7a0327be46a322b38b8d199be45592
rishutonk02 <rishutonk02@gmail.com>
2026-07-30 12:16:00 +0100

    firebase

--- lib/services/firebase_service.dart
+++ lib/services/firebase_service.dart
@@ -1,11 +1,16 @@
 import 'package:firebase_core/firebase_core.dart';
 
 class FirebaseService {
+  FirebaseService._();
+
   static Future<bool> initialise() async {
     try {
-      await Firebase.initializeApp();
+      if (Firebase.apps.isEmpty) {
+        await Firebase.initializeApp();
+      }
       return true;
-    } catch (_) {
+    } catch (error) {
+      print('Firebase initialisation failed: $error');
       return false;
     }
   }
